eb027bf1d86e0b91ebb9953eecfe0ac31348f945
Benjamin Goff <bgoff@mainerobotics.org>
2021-02-26 10:35:05 -0500

    Add project files.

---
+++ DemoJobs/DemoJobs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DemoJobs
+{
+    class DemoJobs
+    {
+        static void Main(string[] args)
+        {
+            Job job1 = new Job();
+            Job job2 = new Job();
+            Job job3 = new Job();
+
+            job1.Description = "Dog Walker";
+            job1.Hourly = 20;
+            job1.Time = 5;
+
+            job2.Description = "Cat Walker";
+            job2.Hourly = 30;
+            job2.Time = 7;
+
+            DisplayJob(job1);
+            DisplayJob(job2);
+            job3 = job1 + job2;
+            DisplayJob(job3);
+
+
+        }
+        public static void DisplayJob(Job j)
+        {
+            Console.WriteLine("{0} rate is {1:C} per an hour. \n" +
+                "If done for {2} hours the Total is: {3:c}",
+                j.Description, j.Hourly, j.Time, j.Total);
+        }
+    }
+}
---
+++ DemoJobs/Job.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace DemoJobs
+{
+    class Job
+    {
+        private string description;
+        private double time;
+        private double hourlyRate;
+        private double total;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public double Time
+        {
+            get
+            {
+                return time;
+            }
+            set
+            {
+                time = value;
+                CalcTotal();
+            }
+        }
+
+        public double Hourly
+        {
+            get
+            {
+                return hourlyRate;
+            }
+            set
+            {
+                hourlyRate = value;
+                CalcTotal();
+            }
+        }
+
+        public double Total
+        {
+            get { return total; }
+        }
+
+        private void CalcTotal()
+        {
+            total = Math.Round((time * hourlyRate), 2, MidpointRounding.AwayFromZero);
+        }
+
+        public static Job operator+(Job j1, Job j2)
+        {
+            Job j3 = new Job();
+
+            j3.Description = j1.Description + " and " + j2.Description;
+            j3.Time = j1.Time + j2.Time;
+            j3.Hourly = (j1.Hourly + j2.Hourly) / 2; 
+
+            return j3;
+        }
+    }
+}
